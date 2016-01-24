0e43fe5e68df6359c540b66b31e559754a33b39f
Avinash Mallik <mallikdav@gmail.com>
2016-01-24 17:49:20 +0530

    fixed import whitenoise error

--- gobble/wsgi.py
+++ gobble/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gobble.settings.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gobble.settings.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
