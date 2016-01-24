f58f917fbe0592cf993aed59c72596894ff021a1
Avinash Mallik <mallikdav@gmail.com>
2016-01-24 15:58:25 +0530

    changing template and static folder path

--- gobble/settings/settings.py
+++ gobble/settings/settings.py
@@ -62,12 +62,11 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'gobble.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, '../templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -130,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
